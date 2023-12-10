a75e7d8c04f8a22d58fe43e8656877f3a59881a3
Thiago <thiagobarroso11@hotmail.com>
2023-12-10 16:37:18 -0300

    ajustes

--- public/js/index.js
+++ public/js/index.js
@@ -26,12 +26,6 @@ if (elemNomeUsuario) {
 
 // -------------
 
-const chk = document.getElementById('chk')
-
-chk.addEventListener('change', () => {
-	document.body.classList.toggle('tema')
-})
-
 function Login() {
 	location.href = "telaLogin.html"
 }
